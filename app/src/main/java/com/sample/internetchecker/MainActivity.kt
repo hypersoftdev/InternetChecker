2c7f87359f264971f2cb4811db9a72e5f7919c4a
Dev15 <dev15@hypersoft.com.pk>
2024-10-02 11:40:35 +0500

    Kamran Ahmed khan

--- app/src/main/java/com/sample/internetchecker/MainActivity.kt
+++ app/src/main/java/com/sample/internetchecker/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.sample.internetchecker
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.internetcheckerlibrary.InternetChecker
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        testingInternet()
+    }
+
+    private fun testingInternet() {
+        if (InternetChecker.isInternetAvailable(this)) {
+            Toast.makeText(this, "Internet Available", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Internet Not Available", Toast.LENGTH_SHORT).show()
         }
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "InternetChecker"
 include(":app")
+include(":InternetCheckerlibrary")
